cd3b95df02265eeec25b42d43de2fbe1445ef472
IT21319860 <100059519+IT21319860@users.noreply.github.com>
2024-05-05 19:42:31 +0530

    1

--- Frontend/client/frontend-NASA-API/src/component/HeroSection.jsx
+++ Frontend/client/frontend-NASA-API/src/component/HeroSection.jsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
             <h1 className="animate-fade-right animate-once animate-once max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl blue-700 dark:text-white">
              The NASA
             </h1>
-            <p className="animate-fade-right animate-once max-w-7xl mb-6 font-bold text-black-600 lg:mb-8 md:text-lg lg:text-2xl dark:text-gray-400">
+            <p className="animate-fade-right animate-once max-w-7xl mb-6 font-bold text-white-600 lg:mb-8 md:text-lg lg:text-2xl dark:text-gray-400">
             NASA, the National Aeronautics and Space Administration, epitomizes humanity's relentless pursuit of knowledge and exploration. Since its inception in 1958, NASA has been the driving force behind our quest to unravel the mysteries of the cosmos and push the boundaries of scientific discovery.
              With a multifaceted mandate encompassing space exploration, scientific research, and aeronautics and aerospace advancements, NASA's endeavors transcend the realm of mere science; they embody the very essence of human ingenuity and aspiration. Through its awe-inspiring missions, from the historic Apollo moon landings to the ongoing exploration of Mars and beyond,
               NASA captivates our imagination and inspires generations to dream boldly and reach for the stars.
